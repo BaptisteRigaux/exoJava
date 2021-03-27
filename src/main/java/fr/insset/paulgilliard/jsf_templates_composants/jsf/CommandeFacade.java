65a0340d0aa9e2526d8e481c22eda0fbe1bdcba1
paulgilliard <paul.gilliard.8@gmail.com>
2021-03-27 13:27:44 +0100

    Question 1 et 2

---
+++ src/main/java/fr/insset/paulgilliard/jsf_templates_composants/jsf/CommandeFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insset.paulgilliard.jsf_templates_composants.jsf;
+
+import fr.insset.paulgilliard.jsf_templates_composants.entités.Commande;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author paul
+ */
+@Stateless
+public class CommandeFacade extends AbstractFacade<Commande> {
+
+    @PersistenceContext(unitName = "my_persistence_unit")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CommandeFacade() {
+        super(Commande.class);
+    }
+    
+}
